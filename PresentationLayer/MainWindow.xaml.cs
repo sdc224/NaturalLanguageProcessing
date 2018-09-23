c77c8cf3ebc6c3a1d3ce78f059bae37dc111dc9f
Sourodeep Chatterjee <sourodeep224@outlook.com>
2018-09-24 03:03:51 +0530

    AddedRecordingPropertyInWavFormat

--- PresentationLayer/MainWindow.xaml.cs
+++ PresentationLayer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
-﻿using IronPython.Hosting;
-using System;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -10,9 +11,15 @@ namespace PresentationLayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string command, StringBuilder retString, int returnLength,
+            IntPtr callBack);
+
         public MainWindow()
         {
             InitializeComponent();
+            ButtonStop.Visibility = Visibility.Hidden;
+            mciSendString("open new Type waveaudio Alias recsound", null, 0, IntPtr.Zero);
         }
 
         public void ChangeImage(Button button)
@@ -22,7 +29,7 @@ namespace PresentationLayer
 
         private void ButtonMic1On_Click(object sender, RoutedEventArgs e)
         {
-            var py = Python.CreateEngine();
+            /*var py = Python.CreateEngine();
 
             try
             {
@@ -32,9 +39,19 @@ namespace PresentationLayer
             }
             catch (Exception exception)
             {
-                MessageBox.Show(exception.ToString());
+                MessageBox.Show(exception.Message);
                 throw;
-            }
+            }*/
+
+            mciSendString("record recsound", null, 0, IntPtr.Zero);
+            ButtonStop.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonStop_OnClick(object sender, RoutedEventArgs e)
+        {
+            mciSendString("save recsound d:\\Games\\mic.wav", null, 0, IntPtr.Zero);
+            mciSendString("close recSound", null, 0, IntPtr.Zero);
+            ButtonStop.Visibility = Visibility.Hidden;
         }
     }
 }

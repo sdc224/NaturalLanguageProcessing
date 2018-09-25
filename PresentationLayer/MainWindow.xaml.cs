c69bbd8bf70a80e1029e2d3565fbba078f6e148e
Sourodeep Chatterjee <sourodeep224@outlook.com>
2018-09-25 20:00:56 +0530

    SomeNormalChangesOnUI

--- PresentationLayer/MainWindow.xaml.cs
+++ PresentationLayer/MainWindow.xaml.cs
@@ -18,7 +18,6 @@ namespace PresentationLayer
         public MainWindow()
         {
             InitializeComponent();
-            ButtonStop.Visibility = Visibility.Hidden;
             mciSendString("open new Type waveaudio Alias recsound", null, 0, IntPtr.Zero);
         }
 
@@ -27,8 +26,9 @@ namespace PresentationLayer
             throw new NotImplementedException();
         }
 
-        private void ButtonMic1On_Click(object sender, RoutedEventArgs e)
+        private void ButtonStartOn_Click(object sender, RoutedEventArgs e)
         {
+            if (!(sender is Button button)) return;
             /*var py = Python.CreateEngine();
 
             try
@@ -44,14 +44,104 @@ namespace PresentationLayer
             }*/
 
             mciSendString("record recsound", null, 0, IntPtr.Zero);
-            ButtonStop.Visibility = Visibility.Visible;
+
+            switch (button.Name)
+            {
+                case "ButtonMic1On":
+                    ButtonMic1Off.IsEnabled = true;
+                    ButtonMic1On.IsEnabled = false;
+                    break;
+
+                case "ButtonMic2On":
+                    ButtonMic2Off.IsEnabled = true;
+                    ButtonMic2On.IsEnabled = false;
+                    break;
+
+                case "ButtonMic3On":
+                    ButtonMic3Off.IsEnabled = true;
+                    ButtonMic3On.IsEnabled = false;
+                    break;
+
+                case "ButtonMic4On":
+                    ButtonMic4Off.IsEnabled = true;
+                    ButtonMic4On.IsEnabled = false;
+                    break;
+
+                case "ButtonMic5On":
+                    ButtonMic5Off.IsEnabled = true;
+                    ButtonMic5On.IsEnabled = false;
+                    break;
+
+                case "ButtonMic6On":
+                    ButtonMic6Off.IsEnabled = true;
+                    ButtonMic6On.IsEnabled = false;
+                    break;
+
+                case "ButtonMic7On":
+                    ButtonMic7Off.IsEnabled = true;
+                    ButtonMic7On.IsEnabled = false;
+                    break;
+
+                case "ButtonMic8On":
+                    ButtonMic8Off.IsEnabled = true;
+                    ButtonMic8On.IsEnabled = false;
+                    break;
+
+                default:
+                    throw new InvalidOperationException("Invalid Button selected");
+            }
         }
 
         private void ButtonStop_OnClick(object sender, RoutedEventArgs e)
         {
             mciSendString("save recsound d:\\Games\\mic.wav", null, 0, IntPtr.Zero);
             mciSendString("close recSound", null, 0, IntPtr.Zero);
-            ButtonStop.Visibility = Visibility.Hidden;
+            if (!(sender is Button button)) return;
+            switch (button.Name)
+            {
+                case "ButtonMic1Off":
+                    ButtonMic1On.IsEnabled = true;
+                    ButtonMic1Off.IsEnabled = false;
+                    break;
+
+                case "ButtonMic2Off":
+                    ButtonMic2On.IsEnabled = true;
+                    ButtonMic2Off.IsEnabled = false;
+                    break;
+
+                case "ButtonMic3Off":
+                    ButtonMic3On.IsEnabled = true;
+                    ButtonMic3Off.IsEnabled = false;
+                    break;
+
+                case "ButtonMic4Off":
+                    ButtonMic4On.IsEnabled = true;
+                    ButtonMic4Off.IsEnabled = false;
+                    break;
+
+                case "ButtonMic5Off":
+                    ButtonMic5On.IsEnabled = true;
+                    ButtonMic5Off.IsEnabled = false;
+                    break;
+
+                case "ButtonMic6Off":
+                    ButtonMic6On.IsEnabled = true;
+                    ButtonMic6Off.IsEnabled = false;
+                    break;
+
+                case "ButtonMic7Off":
+                    ButtonMic7On.IsEnabled = true;
+                    ButtonMic7Off.IsEnabled = false;
+                    break;
+
+                case "ButtonMic8Off":
+                    ButtonMic8On.IsEnabled = true;
+                    ButtonMic8Off.IsEnabled = false;
+                    break;
+
+                default:
+                    throw new InvalidOperationException("Invalid Button selected");
+            }
         }
     }
 }

366b97febcfe9117285e3e923b87dfc75522afc6
Sourodeep Chatterjee <sourodeep224@outlook.com>
2018-10-02 14:07:56 +0530

    DataBase created with Code First with migration initial model, changed Images to Resource/Images

---
+++ PresentationLayer/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+namespace LanguageProcessor.Model
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string Location { get; set; }
+
+        public string HostName { get; set; }
+
+        public IPAddress IpAddress { get; set; }
+    }
+}
---
+++ PresentationLayer/ViewModel/LanguageDbContext.cs
@@ -0,0 +1,10 @@
+﻿using LanguageProcessor.Model;
+using System.Data.Entity;
+
+namespace LanguageProcessor.ViewModel
+{
+    class LanguageDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
